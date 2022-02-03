c8b6c367af36e12cf52fc0bd4783bedf21af0608
LeoNielsen <leo.nielsen21@gmail.com>
2022-02-03 16:09:05 +0100

    laksdmflp

---
+++ src/test/java/Persistence/UserMapperTest.java
@@ -0,0 +1,13 @@
+package Persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserMapperTest {
+
+    @Test
+    void getAllUserNames() {
+        assertEquals(1,1);
+    }
+}
