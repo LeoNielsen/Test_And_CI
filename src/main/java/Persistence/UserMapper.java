bb137a1b76c74d3e4e1ef2b07c975623f11208c9
LeoNielsen <leo.nielsen21@gmail.com>
2022-02-03 15:34:32 +0100

    added getAllUserNames

---
+++ src/main/java/Persistence/UserMapper.java
@@ -0,0 +1,32 @@
+package Persistence;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserMapper {
+
+    public ArrayList<String> getAllUserNames() {
+        ArrayList<String> names = new ArrayList<>();
+        try (Connection connection = DBConnector.connection()){
+            String SQL = "Select fname from usertable";
+            try (PreparedStatement ps = connection.prepareStatement(SQL)){
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    String fname =rs.getString("fname");
+                    names.add(fname);
+                }
+                return names;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+}
